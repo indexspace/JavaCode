a29478a7f9ca16f27864ae3a6de8d7c0461d0083
indexspace <2838839917@qq.com>
2023-06-20 17:51:01 +0800

    6.20

--- demo/src/Main.java
+++ demo/src/Main.java
@@ -9,7 +9,19 @@ public class Main {
         System.out.println("002");
         System.out.println("003");
         System.out.println("005");
+        System.out.println(zero());
+        System.out.println(zero1());
+        System.out.println(zero());
+        System.out.println(zero1());
 
+    }
+    
+    static int zero() {
+        return 0;
+    }
+    
+    static int zero1() {
+        return 1;
     }
 
 }
