6b8b4b7eea1f942ca9820d974bba1a7698f14242
indexspace <2838839917@qq.com>
2023-09-22 18:24:40 +0800

    servlet 实验一 + 作业

---
+++ Servlet/src/main/java/org/czp/servlet/ServletDemo.java
@@ -0,0 +1,29 @@
+package org.czp.servlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class ServletDemo implements Servlet {
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        String name = servletRequest.getParameter("name");
+        servletResponse.getWriter().write("<H1>hello, "+name+"</H1>");
+    }
+
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    public String getServletInfo() {
+        return null;
+    }
+
+    public void destroy() {
+
+    }
+}
